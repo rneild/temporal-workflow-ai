09cc877ef459b0becd3fe22ab1833d6f66c51df2
rneild (home) <robert.neild@gmail.com>
2026-05-09 08:47:15 -0700

    feat(perfect_cup_of_coffee-83eb42): perfect_cup_of_coffee

--- perfect_cup_of_coffee-83eb42/worker.py
+++ perfect_cup_of_coffee-83eb42/worker.py
@@ -4,7 +4,8 @@ from temporalio.client import Client
 from workflow import PerfectCupOfCoffeeWorkflow
 from activities import (
     select_beans, grind_beans, heat_water, prep_brewer,
-    dose_coffee, bloom_pour, main_brew, taste_eval, choose_milk,
+    dose_coffee, bloom_pour, main_brew, taste_eval,
+    add_sugar, choose_milk,
 )
 
 async def main():
@@ -15,7 +16,8 @@ async def main():
         workflows=[PerfectCupOfCoffeeWorkflow],
         activities=[
             select_beans, grind_beans, heat_water, prep_brewer,
-            dose_coffee, bloom_pour, main_brew, taste_eval, choose_milk,
+            dose_coffee, bloom_pour, main_brew, taste_eval,
+            add_sugar, choose_milk,
         ],
     )
     await worker.run()
